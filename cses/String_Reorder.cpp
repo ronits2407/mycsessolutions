9d7276e7bba6bb37abb1f1bc762c6562c7e76597
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:11:25 +0530

    Backfill CSES solution for String Reorder

---
+++ cses/String_Reorder.cpp
@@ -0,0 +1,10 @@
+input/code.cpp: In function 'void solve_ronit()':
+input/code.cpp:40:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   40 |     for (int i = 0; i < s.size(); i++)
+      |                     ~~^~~~~~~~~~
+input/code.cpp:47:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   47 |     for (int i = 0; i < s.size(); i++)
+      |                     ~~^~~~~~~~~~
+input/code.cpp:83:24: warning: comparison of integer expressions of different signedness: 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} and 'long long int' [-Wsign-compare]
+   83 |         if (ans.size() != i + 1)
+      |             ~~~~~~~~~~~^~~~~~~~
