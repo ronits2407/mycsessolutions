bda6a1c225a2a289be16e12a15d5aa8af4dd8e84
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:10:52 +0530

    Backfill CSES solution for Bit Strings

---
+++ cses/Bit_Strings.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '\n';
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]\n"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+
+
+int32_t main()
+{
+    cout << fixed << setprecision(20);
+
+    int n;
+    cin >> n;
+
+    int ans  = 1;
+    for (int i = 0; i < n; i++)
+    {
+        ans = (ans * 2) % MOD;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
