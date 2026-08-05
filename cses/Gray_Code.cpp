4c77f2edc6f0be56eeee59dc8eadead1a6136c2a
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:11:02 +0530

    Backfill CSES solution for Gray Code

---
+++ cses/Gray_Code.cpp
@@ -0,0 +1,4 @@
+input/code.cpp: In function 'int32_t main()':
+input/code.cpp:73:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::vector<std::__cxx11::basic_string<char> >::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   73 |     for (int i = 0; i < ans.size(); i++)
+      |                     ~~^~~~~~~~~~~~
