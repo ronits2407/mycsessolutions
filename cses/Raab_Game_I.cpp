2e09ead6e88ffb845212d1249378c2ff127c3cb6
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:11:14 +0530

    Backfill CSES solution for Raab Game I

---
+++ cses/Raab_Game_I.cpp
@@ -0,0 +1,7 @@
+input/code.cpp: In function 'void solve_ronit(long long int)':
+input/code.cpp:63:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::vector<std::pair<long long int, long long int> >::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   63 |     for (int i = 0; i < ans.size(); i++)
+      |                     ~~^~~~~~~~~~~~
+input/code.cpp:68:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::vector<std::pair<long long int, long long int> >::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   68 |     for (int i = 0; i < ans.size(); i++)
+      |                     ~~^~~~~~~~~~~~
