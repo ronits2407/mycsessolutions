6083f4ce30295bfbcf3a74890006224020bad3e6
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:11:39 +0530

    Backfill CSES solution for Restaurant Customers

---
+++ cses/Restaurant_Customers.cpp
@@ -0,0 +1,4 @@
+input/code.cpp: In function 'void solve_ronit()':
+input/code.cpp:52:23: warning: comparison of integer expressions of different signedness: 'long long int' and 'std::vector<std::pair<long long int, long long int> >::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   52 |     for (int i = 0; i < a.size(); i++)
+      |                     ~~^~~~~~~~~~
