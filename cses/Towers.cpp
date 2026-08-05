47646ca4151041765b3ffa54a60d24a6298cb95a
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-05 19:11:59 +0530

    Backfill CSES solution for Towers

---
+++ cses/Towers.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '\n';
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]\n"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+
+void solve_ronit()
+{
+    int n;
+    cin >> n;
+
+    vi a(n);
+    ask(a, n);
+
+    multiset<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        auto it = s.lower_bound(a[i]+1);
+        if (it != s.end())
+        {
+            debug(*it);
+            s.erase(it);
+            s.insert(a[i]);
+        }
+        else
+        {
+            s.insert(a[i]);
+        }
+        
+        
+    }
+    cout << s.size() << endl;
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(20);
+    solve_ronit();
+    return 0;
+}
