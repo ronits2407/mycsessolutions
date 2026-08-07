243d29dd2b9d425fafeba0f94e02c8c3fd157405
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-07 21:47:41 +0530

    Add CSES solution for Tasks_and_Deadlines

---
+++ cses/Tasks_and_Deadlines.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+ 
+// PBDS headers
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+// PBDS indexed_set definition
+typedef tree<
+    int, 
+    null_type, 
+    less<int>, 
+    rb_tree_tag, 
+    tree_order_statistics_node_update> 
+    indexed_set;
+ 
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+ 
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '\n';
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]\n"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+ 
+void solve_ronit()
+{
+    int n;
+    cin >> n;
+ 
+    vpii a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].first;
+        cin >> a[i].second;
+ 
+    }
+ 
+    sort(all(a));
+    
+    int score = 0;
+    int time = 0;
+    for (int i = 0; i < n; i++)
+    {
+        time += a[i].first;
+        score += a[i].second- time;
+    }
+    cout << score << '\n'; 
+    
+}
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(20);
+    solve_ronit();
+    return 0;
+}
