ea8ee1560bd8029b4653f7019acbdbf947ecfd35
Ronit Sonawane <ronitsonawane2007@gmail.com>
2026-08-11 17:29:07 +0530

    Add CSES solution for Coin_Combinations_I

---
+++ cses/Coin_Combinations_I.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+ 
+// PBDS headers
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+// PBDS indexed_set definition
+typedef tree<
+    int, 
+    null_type, 
+    less<int>, 
+    rb_tree_tag, 
+    tree_order_statistics_node_update> 
+    indexed_set;
+ 
+#define int long long
+#define pii pair<int, int>
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define ask(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cin >> v[i];
+ 
+#define give(v, n) \
+    for (int i = 0; i < (n); ++i) \
+        cout << v[i] << ' '; \
+    cout << '\n';
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void _print() { cerr << "]\n"; }
+template <typename T, typename... Args>
+void _print(T t, Args... args)
+{
+    cerr << t << (sizeof...(args) ? ", " : "");
+    _print(args...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...) \
+    cerr << "[" << #x << "] = ["; \
+    _print(x);
+#else
+#define debug(x...)
+#endif
+ 
+void solve_ronit()
+{
+    int n, x;
+    cin >> n >> x;
+    vi a(n);
+    ask(a, n);
+    set<int> coins;
+    for (int i = 0; i < n; i++)
+    {
+        coins.insert(a[i]);
+    }
+    
+    vi dp(x+1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        for (auto &&ele : coins)
+        {
+            if (i - ele >= 0)
+            {
+                dp[i] += dp[i-ele];
+            }
+        }
+    }
+    cout << dp[x];
+}
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(20);
+    solve_ronit();
+    return 0;
+}
